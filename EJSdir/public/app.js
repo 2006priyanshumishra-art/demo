57fc0351601db80f3b5ed6e54cd93850d5d38adf
2006priyanshumishra-art <2006priyanshumishra@gmail.com>
2026-09-13 23:30:30 +0530

    This is the use of Node.js, Express.js and EJS

---
+++ EJSdir/public/app.js
@@ -0,0 +1,8 @@
+btns = document.querySelectorAll("button");
+
+for(let btn of btns){
+    btn.addEventListener("click", ()=>{
+        console.log("Button Clicked");
+    });
+    
+}
---
+++ ExpressDir/index.js
@@ -0,0 +1,34 @@
+// const express = require("express");
+// const app = express();
+
+// let port = 3000;
+
+// app.listen(port,() =>{
+//     console.log(`App is listening on port ${port}`);
+// })
+
+// app.get("/",(req,res)=>{
+//     res.send("Hello i m root")
+// })
+
+// app.get("/:username/:id",(req,res)=>{
+//     let {username,id} = req.params;
+//     h= `<h1>Welcome to @${username}</h1>`;
+//     res.send(h);
+// })
+
+// app.get("/search",(req,res)=>{
+//     console.log(req.query);
+//     res.send("No results");
+// })
+
+
+// // app.use((req,res)=>{
+// //     // console.log(req);
+// //     console.log("incomming req");
+// //     res.send({
+// //         name : "apple",
+// //         color : "red"
+// //     });
+// // })
+
